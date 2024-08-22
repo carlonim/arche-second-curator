31f00f3addb5be328e3f8a4a75e6e28c381bc3df
Massimiliano Carloni <massimiliano.carloni@oeaw.ac.at>
2024-08-22 15:16:30 +0200

    Create html_to_df.py

---
+++ html_to_df.py
@@ -0,0 +1,25 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://arche.acdh.oeaw.ac.at/browser/api/getRootTable/en?_format=html"
+
+root_table = urllib.request.urlopen(url).read()
+
+soup = BeautifulSoup(root_table, 'html.parser')
+
+columns = []
+
+for cell in soup.table.contents[0].contents[0]:
+    columns.append(cell.text)
+
+lists = []
+
+for row in soup.table.contents[1:]:
+    list = []
+    for cell in row.contents:
+        list.append(cell.text)
+    lists.append(list)
+
+df = pd.DataFrame(lists)
+df.columns = columns
